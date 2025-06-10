7a78445f7beef7dceaec293d0150d093a159553c
BuzzLightYear <87924371+WYan202@users.noreply.github.com>
2025-06-10 14:25:23 +0900

    Update sample9.php

--- sample9.php
+++ sample9.php
@@ -1,4 +1,24 @@
-if (isset($_POST['name'])) {
-    echo '送信された値：' . htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
-}
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>名前送信フォーム</title>
+</head>
+<body>
 
+<?php
+// 送信データがあるか確認
+if (isset($_POST['name']) && $_POST['name'] !== '') {
+    $name = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
+    echo "<h2>送信された値：{$name}</h2>";
+}
+?>
+
+<form method="post" action="sample8.php">
+    <label>名前：</label>
+    <input type="text" name="name">
+    <input type="submit" value="送信">
+</form>
+
+</body>
+</html>
